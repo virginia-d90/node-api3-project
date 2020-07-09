3a9b255e2aae7e17f4bf5c4d67ae407d744a8b87
Virginia Davenport <virginialdport@gmail.com>
2020-07-09 13:26:20 -0600

    ready to deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require("./server")
 
 
 
-const PORT = 5000;
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
 })
